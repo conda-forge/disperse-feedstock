769c5c09ecb6cd738e21942f975b0c5e4c1f165a
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-03-30 14:44:31 +0200

    Fix compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,13 @@
 set -euo pipefail
 set -x
 
+# Apply patch
+git apply 001-fix_cmakelist.patch
+
 mkdir -p build
 
 cd build
 
-cmake ${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../
+cmake ${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DUSE_TR1=false ../
 make -j${CPU_COUNT}
 make install
